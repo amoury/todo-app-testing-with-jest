976394b85794f0c00f7faa380c71dd0cfdb7e784
Ansar Memon <ansar.memon@hotmail.com>
2020-04-13 17:43:18 +0400

    Completed: component dynamic count

--- src/components/completed.jsx
+++ src/components/completed.jsx
@@ -1,12 +1,32 @@
 import React from "react";
 
-const Completed = () => {
+const Completed = ({ todos }) => {
+  const completedTodos = todos.filter((todo) => todo.completed);
+
+  const renderMessage = () => {
+    if (completedTodos.length <= 1) {
+      return {
+        title: "Come on you can do it..!",
+        subtitle: `Only ${todos.length} tasks to go`,
+      };
+    } else if (completedTodos.length < todos.length) {
+      return {
+        title: `Awesome you have completed ${completedTodos.length} tasks already`,
+        subtitle: `Only ${todos.length - completedTodos.length} tasks to go`,
+      };
+    } else {
+      return { title: `Excellent you have completed all the tasks today` };
+    }
+  };
+
+  if (!todos.length) return null;
+
   return (
     <div className="completed ui icon message">
       <i className="check circle icon"></i>
       <div className="content">
-        <div className="header">Awesome you have completed 5 tasks today</div>
-        <p>Only 4 more tasks to go</p>
+        <div className="header">{renderMessage().title}</div>
+        {renderMessage().subtitle && <p>{renderMessage().subtitle}</p>}
       </div>
     </div>
   );
