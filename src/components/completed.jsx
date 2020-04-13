5cb3aaa9334f7d6024b616b2d68d20395a683229
Ansar Memon <ansar.memon@hotmail.com>
2020-04-13 12:54:01 +0400

    TodoItem - handle completed tasks

--- src/components/completed.jsx
+++ src/components/completed.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Completed = () => {
   return (
-    <div class="completed ui icon message">
-      <i class="check circle icon"></i>
-      <div class="content">
-        <div class="header">Awesome you have completed 5 tasks today</div>
+    <div className="completed ui icon message">
+      <i className="check circle icon"></i>
+      <div className="content">
+        <div className="header">Awesome you have completed 5 tasks today</div>
         <p>Only 4 more tasks to go</p>
       </div>
     </div>
--- src/components/todo-list.jsx
+++ src/components/todo-list.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import TodoItem from "./todo-list-item";
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, markCompleted }) => {
   return (
     <div className="ui middle aligned divided list todo-list">
       {todos.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} />
+        <TodoItem key={todo.id} todo={todo} markCompleted={markCompleted} />
       ))}
     </div>
   );
