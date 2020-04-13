25d2235131684b89164bc01690543116772652be
Ansar Memon <ansar.memon@hotmail.com>
2020-04-13 12:01:44 +0400

    AddToDo - added focus to input element

--- src/components/add-todo.jsx
+++ src/components/add-todo.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
 const AddTodo = () => {
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
   return (
     <form className="ui form">
       <div className="ui fluid action input">
-        <input type="text" id="new-item" placeholder="Add a new todo" />
+        <input
+          type="text"
+          name="new-item"
+          id="new-item"
+          placeholder="Add a new todo"
+          ref={inputRef}
+        />
         <button type="submit" className="ui button teal">
           Add
         </button>
