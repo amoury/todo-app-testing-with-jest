d5ae761f1dbd316e3c412b045c9aa85cf780bb05
Ansar Memon <ansar.memon@hotmail.com>
2020-04-13 11:52:32 +0400

    completed all the markup

--- src/components/add-todo.jsx
+++ src/components/add-todo.jsx
@@ -4,8 +4,8 @@ const AddTodo = () => {
   return (
     <form className="ui form">
       <div className="ui fluid action input">
-        <input type="text" id="new-item" />
-        <button type="submit" className="ui button">
+        <input type="text" id="new-item" placeholder="Add a new todo" />
+        <button type="submit" className="ui button teal">
           Add
         </button>
       </div>
---
+++ src/components/completed.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Completed = () => {
+  return (
+    <div class="completed ui icon message">
+      <i class="check circle icon"></i>
+      <div class="content">
+        <div class="header">Awesome you have completed 5 tasks today</div>
+        <p>Only 4 more tasks to go</p>
+      </div>
+    </div>
+  );
+};
+
+export default Completed;
